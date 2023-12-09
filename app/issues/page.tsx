76c1c1bd5576fb85144f1d5662ed1381bacbb0e7
SangLM-Dev <sanglm2207@gmail.com>
2023-12-09 23:52:15 +0700

    add list issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,12 +1,47 @@
+'use client';
 
-import React from 'react'
-import { Button } from '@radix-ui/themes';
+import React, { useEffect, useState } from 'react'
+import { Button, Table } from '@radix-ui/themes';
 import Link from 'next/link';
 
+interface Issue {
+  title: string;
+  description: string;
+}
+
 const IssuesPage = () => {
+  const [issues, setIssues] = useState<Issue[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('/api/issues');
+      const data = await response.json();
+      
+      setIssues(data);
+    };
+    fetchData();
+  }, [issues]);
+
   return (
     <div>
-        <Button><Link href='/issues/new'>New Issue</Link></Button>
+      <Button><Link href='/issues/new'>New Issue</Link></Button>
+      <Table.Root>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Title</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Description</Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {issues.map((issue, index) => (
+            <Table.Row key={index}>
+              <Table.RowHeaderCell>{issue.title}</Table.RowHeaderCell>
+              <Table.Cell>{issue.description}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
     </div>
   )
 }
