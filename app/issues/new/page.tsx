52a0d32e6c3378c275748c53b4b15fefca1c4f0e
SangLM-Dev <sanglm2207@gmail.com>
2023-12-09 22:53:27 +0700

    Implement client-side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,21 +1,27 @@
 'use client';
 
-import { Button, Callout, TextArea, TextField } from '@radix-ui/themes';
+import { Button, Callout, Text, TextArea, TextField } from '@radix-ui/themes';
 import React, { useState } from 'react'
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/validationSchemas';
+import { z } from 'zod';
+// interface InssueForm {
+//     title: string;
+//     description: string;
+// }
 
-interface InssueForm {
-    title: string;
-    description: string;
-}
+type InssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
     const router = useRouter();
-    const { register, control, handleSubmit } = useForm<InssueForm>();
+    const { register, control, handleSubmit, formState: { errors } } = useForm<InssueForm>({
+        resolver: zodResolver(createIssueSchema)
+    });
     const [error, setError] = useState('');
 
     return (
@@ -34,11 +40,14 @@ const NewIssuePage = () => {
                 <TextField.Root>
                     <TextField.Input placeholder="Title" {...register('title')} />
                 </TextField.Root>
+                {errors.title && <Text color='red' as='p'>{errors.title.message}</Text>}
                 <Controller
                     name='description'
                     control={control}
                     render={({ field }) => <SimpleMDE placeholder='Description' {...field} />}
                 />
+                {errors.description && <Text color='red' as='p'>{errors.description.message}</Text>}
+                
                 <Button>Submit</Button>
             </form>
         </div>
